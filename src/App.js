ffd8745ec617925b7d4cf3d1fa1d3c50d702c2c5
Fiona Voss <fionavoss@gmail.com>
2017-08-12 11:36:56 -0700

    Render board with 9 question marks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Square } from './Square';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Find the Loot</h2>
+        <table style={{fontSize: 100}}>
+          <tr>
+            <Square/>
+            <Square/>
+            <Square/>
+          </tr>
+          <tr>
+            <Square/>
+            <Square/>
+            <Square/>
+          </tr>
+          <tr>
+            <Square/>
+            <Square/>
+            <Square/>
+          </tr>
+        </table>
       </div>
     );
   }
---
+++ src/Square.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Square extends Component {
+  render() {
+    return (
+      <td style={{ height: 200, width: 200}}>&#10067;</td>
+    )
+  }
+}
